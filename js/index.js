f98ad974e60a6bc48161d2bce234d51873087c88
Hannes O <hannesottt@gmail.com>
2019-12-09 10:52:00 +0200

    add local storage, deletion unfinished

--- js/index.js
+++ js/index.js
@@ -2,40 +2,76 @@ var elements = document.getElementsByClassName("removeButton");
 const taskList = document.getElementById("taskList");
 const removeButtonInner = '<span class="cursorPointer">✖</span>';
 
-for (let i = 0; i < elements.length; i++) {
-    const element = elements[i];
-    addRemoveOnClick(element);
+var items = new Array();
+var localStorage = window.localStorage;
+var itemsLocal = localStorage.getItem("items");
+if (!itemsLocal) {
+    localStorage.setItem("items", items);
+} else {
+    loadLocalStorage();
 }
 
 function addRemoveOnClick(element) {
-    element.onclick = function() {
-        element.parentNode.parentNode.removeChild(element.parentNode);
-    };
+    //ugly
+    /*assumes, that we're organized like this:
+    <tbody>
+        <tr>
+            <td>
+                <span> <-onclick is added here*/
+    if (typeof element === "object" && element.nodeName === "SPAN") {
+        element.onclick = function() {
+            element.parentNode.parentNode.parentNode.removeChild(element.parentNode.parentNode);
+            removeFromLocalStorage();
+        };
+    }
 }
 
 const submitButton = document.getElementById("submitItem");
 
 submitButton.onclick = () => {
-    let item = document.getElementById("listTextInput").value;
-    if (item != "") {
-        //create remove button
-        var removeButton = document.createElement("td");
-        removeButton.classList.add("removeButton");
-        removeButton.innerHTML = removeButtonInner;
-        addRemoveOnClick(removeButton);
-
-        //create task item
-        var task = document.createElement("td");
-        task.innerText = item;
-
-        //put them together in one row
-        var row = document.createElement("tr");
-        row.appendChild(task);
-        row.appendChild(removeButton);
-
-        //add to site
-        taskList.appendChild(row);
+    console.log(document.getElementById("thead").childNodes);
+    let itemText = document.getElementById("listTextInput").value;
+    if (itemText != "") {
+        createListItem(itemText);
+        document.getElementById("listTextInput").value = "";
+        saveToLocalStorage(itemText);
     }
-    
 }
 
+function createListItem(text) {
+    //create remove button
+    var removeButton = document.createElement("td");
+    removeButton.classList.add("removeButton");
+    var removeSpan = document.createElement("span");
+    removeSpan.classList.add("cursorPointer");
+    removeSpan.innerText = "✖";
+    removeButton.appendChild(removeSpan);
+    addRemoveOnClick(removeSpan);
+    //create task item
+    var item = document.createElement("td");
+    item.classList.add("taskItem");
+    item.innerText = text;
+    //put them together in one row
+    var row = document.createElement("tr");
+    row.appendChild(item);
+    row.appendChild(removeButton);
+    //add to site
+    taskList.appendChild(row);
+    //empty text box
+}
+
+function saveToLocalStorage(text) {
+    items.push(text);
+    localStorage.setItem("items", items);
+}
+
+function loadLocalStorage() {
+    items = itemsLocal.split(",");
+    items.forEach(item => {
+        createListItem(item);
+    });
+}
+
+function removeFromLocalStorage(index) {
+    console.log(index);
+}
